7e3a4632e204f993017cd1c070bc1e0d8a20fa68
Olvenn <olga-vencel@yandex.ru>
2022-08-13 17:41:34 +0300

    add utils.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,1 +1,15 @@
-console.log('start');
+#!/usr/bin/env node
+
+console.log('Welcome to the Brain Games!');
+
+const name = readlineSync.question('May I have your name ');
+
+console.log(`Hello, ${name}`);
+
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+console.log(rule);
+
+const numberOfQuestions = 3;
+
+
