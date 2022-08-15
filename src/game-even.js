0c3270787cb1ee4477ab241f446d9c2bccb6869b
Olvenn <olga-vencel@yandex.ru>
2022-08-15 11:44:06 +0300

    change linter mistakes

--- src/game-even.js
+++ src/game-even.js
@@ -1,15 +1,13 @@
-import readlineSync from 'readline-sync';
-import { checkOddOrEven, getRandomNumber } from  './utils.js';
+import { checkOddOrEven, getRandomNumber } from './utils.js';
 import game from './index.js';
 
 const getGameData = () => {
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const number = getRandomNumber(1, 100);
+  const number = getRandomNumber(1, 1000);
   const correctAnswer = checkOddOrEven(number) ? 'yes' : 'no';
-    
   return [rule, number, correctAnswer];
 };
 
-const gameEven = (data) => { return game(getGameData)};
+const gameEven = () => game(getGameData);
 
 export default gameEven;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
 import readlineSync from 'readline-sync';
-import getGameData from './index.js';
-import { checkOddOrEven, getRandomNumber } from  './utils.js';
 
 const game = (getGameData) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name ');
   console.log(`Hello, ${name}!`);
-  const [ rule ] = getGameData();
+  const [rule] = getGameData();
   console.log(rule);
 
   const requestResponse = 'Your answer: ';
   const correct = 'Correct!';
-	    
   const numberOfMoves = 3;
-	  	
   for (let i = 0; i < numberOfMoves; i += 1) {
     const [, questionData, correctAnswer] = getGameData();
     console.log(`Question: ${questionData}`);
     const answer = readlineSync.question(requestResponse);
-    if(correctAnswer === answer) {
-       console.log(correct);
-     } else {
-       return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${name}!`);
-     }	  
-   };
+    if (correctAnswer === answer) {
+      console.log(correct);
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${name}!`);
+    }
+  }
   return console.log(`Congratulations, ${name}!`);
 };
-
 export default game;
-
