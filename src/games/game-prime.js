a9e202f8fc748c8e41b3f83a2e04ea4e2e560285
Olvenn <olga-vencel@yandex.ru>
2022-08-16 12:36:17 +0300

    start brain-prime game

---
+++ src/games/game-prime.js
@@ -0,0 +1,13 @@
+import { checkOddOrEven, getRandomNumber } from '../utils.js';
+import game from '../index.js';
+
+const getGameData = () => {
+	  const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+	  const number = getRandomNumber(1, 1000);
+	  const correctAnswer = checkOddOrEven(number) ? 'yes' : 'no';
+	  return [rule, number, correctAnswer];
+};
+
+const gameEven = () => game(getGameData);
+
+export default gameEven;
