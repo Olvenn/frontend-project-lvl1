b35b6e1f6ad2e7327bb3ac897a72b68ce765e17d
Olvenn <olga-vencel@yandex.ru>
2022-08-16 23:06:10 +0300

    removing the magic numbers

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -3,8 +3,8 @@ import game from '../index.js';
 
 const getGameData = () => {
   const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const MIN_NUMBER = 1;
-  const MAX_NUMBER = 1000;
+  const MIN_NUMBER = 3;
+  const MAX_NUMBER = 250;
   const number = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
   const correctAnswer = isPrimeNum(number) ? 'yes' : 'no';
   return [rule, number, correctAnswer];
