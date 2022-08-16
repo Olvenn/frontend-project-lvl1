719a7bd03fbe6b4496c209674cb8b1e91d405d16
Olvenn <olga-vencel@yandex.ru>
2022-08-16 23:13:59 +0300

    duplicate code

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -2,9 +2,9 @@ import { isPrimeNum, getRandomNumber } from '../utils.js';
 import game from '../index.js';
 
 const getGameData = () => {
-  const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const MIN_NUMBER = 3;
   const MAX_NUMBER = 250;
+  const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const number = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
   const correctAnswer = isPrimeNum(number) ? 'yes' : 'no';
   return [rule, number, correctAnswer];
