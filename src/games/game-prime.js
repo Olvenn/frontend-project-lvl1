37b48176c04c6117b08f0876b70e8f7949110b98
Olvenn <olga-vencel@yandex.ru>
2022-08-16 21:57:42 +0300

    finish prime game

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,13 +1,13 @@
-import { checkOddOrEven, getRandomNumber } from '../utils.js';
+import { isPrimeNum, getRandomNumber } from '../utils.js';
 import game from '../index.js';
 
 const getGameData = () => {
-	  const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-	  const number = getRandomNumber(1, 1000);
-	  const correctAnswer = checkOddOrEven(number) ? 'yes' : 'no';
-	  return [rule, number, correctAnswer];
+  const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const number = getRandomNumber(1, 1000);
+  const correctAnswer = isPrimeNum(number) ? 'yes' : 'no';
+  return [rule, number, correctAnswer];
 };
 
-const gameEven = () => game(getGameData);
+const brainPrime = () => game(getGameData);
 
-export default gameEven;
+export default brainPrime;
