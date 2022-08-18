98c07da7e129d97136c8f55334bb347a1e5d167f
Olvenn <olga-vencel@yandex.ru>
2022-08-18 18:59:53 +0300

    fixed utils.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from '../utils.js';
+import getRandomNumber from '../utils.js';
 import mainGameLogic from '../index.js';
 
 const isCheckOddOrEven = (number) => number % 2 === 0;
