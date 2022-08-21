9314df7684999cd6906d1eef8fef76d3ef6a88db
Olvenn <olga-vencel@yandex.ru>
2022-08-21 15:29:00 +0300

    divided the function into two, corrected the name of the function isCheckOddOrEven

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,14 +1,14 @@
 import getRandomNumber from '../utils.js';
 import mainGameLogic from '../index.js';
 
-const isCheckOddOrEven = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const getGameData = () => {
   const MIN_NUMBER = 1;
   const MAX_NUMBER = 1000;
   const number = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-  const correctAnswer = isCheckOddOrEven(number) ? 'yes' : 'no';
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
   return [number, correctAnswer];
 };
 
--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,29 +1,32 @@
 import getRandomNumber from '../utils.js';
 import mainGameLogic from '../index.js';
 
-const makeProgression = (length, step, firstNumber) => {
+const PROGRESS_LENGTH = 10;
+const MIN_STEP = 2;
+const MAX_STEP = 5;
+const MIN_START = 3;
+const MAX_START = 6;
+const stepper = getRandomNumber(MIN_STEP, MAX_STEP);
+const startNumber = getRandomNumber(MIN_START, MAX_START);
+const rawProgression = (length, step, firstNumber) => {
   const progressionFull = [firstNumber];
-  const emptyIndex = getRandomNumber(0, length);
-
   for (let i = 0; i < length; i += 1) {
     progressionFull.push((progressionFull[progressionFull.length - 1]) + step);
   }
-  const emptyNumber = progressionFull[emptyIndex];
-  progressionFull[emptyIndex] = '..';
-  const progression = progressionFull.join(' ');
+  return progressionFull;
+};
+const getProgressionAndNumber = (makeProgression) => {
+  let progression = makeProgression;
+  const emptyIndex = getRandomNumber(0, makeProgression.length - 1);
+  const emptyNumber = makeProgression[emptyIndex];
+  progression[emptyIndex] = '..';
+  progression = progression.join(' ');
   return [progression, emptyNumber];
 };
-
 const gameRule = 'What number is missing in the progression?';
 const getGameData = () => {
-  const PROGRESS_LENGTH = 10;
-  const MIN_STEP = 2;
-  const MAX_STEP = 5;
-  const MIN_START = 3;
-  const MAX_START = 6;
-  const step = getRandomNumber(MIN_STEP, MAX_STEP);
-  const startNumber = getRandomNumber(MIN_START, MAX_START);
-  const [progression, correctAnswer] = makeProgression(PROGRESS_LENGTH, step, startNumber);
+  const progressionFull = rawProgression(PROGRESS_LENGTH, stepper, startNumber);
+  const [progression, correctAnswer] = getProgressionAndNumber(progressionFull);
   return [progression, correctAnswer.toString()];
 };
 
